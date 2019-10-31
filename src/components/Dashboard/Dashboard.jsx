8b099863bfaf5db13f37348f052787a2f1fd8308
Mark Artishuk <markyshuk@gmail.com>
2019-10-31 12:51:53 -0700

    Added basic display and dashboard components with styling

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Dashboard.css";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <button>Strike</button>
+      <button>Ball</button>
+      <button>Foul</button>
+      <button>Hit</button>
+    </div>
+  );
+};
+
+export default Dashboard;
